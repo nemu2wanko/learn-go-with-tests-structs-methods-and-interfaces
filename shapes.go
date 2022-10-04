0f2023bee096b73c0a4933ddffc7d7643c8ff1fa
Nobuchika Tanaka <sleepingdog.7799@gmail.com>
2022-10-05 06:46:02 +0900

    Add Area()

--- shapes.go
+++ shapes.go
@@ -4,3 +4,8 @@ package main
 func Perimeter(width float64, height float64) float64 {
 	return (width + height) * 2
 }
+
+// Area returns the area of a rectangle.
+func Area(width float64, height float64) float64 {
+	return width * height
+}
